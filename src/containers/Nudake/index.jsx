68a43f20adea95d628f9a1304ee2795620378384
hyojinLeee-git <splato8899@gmail.com>
2023-10-22 20:05:45 +0900

    feat: 화면 이미지 지우는 기능 추가

--- src/containers/Nudake/index.jsx
+++ src/containers/Nudake/index.jsx
@@ -4,6 +4,7 @@ import "./style.css";
 import image1 from "../../assets/nudake-1.jpg";
 import image2 from "../../assets/nudake-2.jpg";
 import image3 from "../../assets/nudake-3.jpg";
+import { getAngle, getDistance } from "../../utils/util";
 
 const Nudake = () => {
     const canvasRef = useRef(null);
@@ -14,6 +15,7 @@ const Nudake = () => {
 
         const imageSrcs = [image1, image2, image3];
         let currIndex = 0;
+        let prevPos = { x: 0, y: 0 };
 
         let canvasWidth, canvasHeight;
 
@@ -37,10 +39,50 @@ const Nudake = () => {
             };
         }
 
+        function onMouseDown(e) {
+            canvas.addEventListener("mouseup", onMouseUp);
+            canvas.addEventListener("mouseleave", onMouseUp);
+            canvas.addEventListener("mousemove", onMouseMove);
+            prevPos = { x: e.offsetX, y: e.offsetY };
+        }
+
+        function onMouseUp() {
+            canvas.removeEventListener("mouseup", onMouseUp);
+            canvas.removeEventListener("mouseleave", onMouseUp);
+            canvas.removeEventListener("mousemove", onMouseMove);
+        }
+
+        function onMouseMove(e) {
+            drawCircles(e);
+        }
+
+        function drawCircles(e) {
+            const nextPos = { x: e.offsetX, y: e.offsetY };
+            const dist = getDistance(prevPos, nextPos);
+            const angle = getAngle(prevPos, nextPos);
+
+            for (let i = 0; i < dist; i++) {
+                const x = prevPos.x + Math.cos(angle) * i;
+                const y = prevPos.y + Math.sin(angle) * i;
+
+                ctx.globalCompositeOperation = "destination-out";
+                ctx.beginPath();
+                ctx.arc(x, y, 50, 0, Math.PI * 2);
+                ctx.fill();
+                ctx.closePath();
+            }
+
+            prevPos = nextPos;
+        }
+
+        canvas.addEventListener("mousedown", onMouseDown);
+
         window.addEventListener("resize", resize);
         resize();
 
         return () => {
+            canvas.removeEventListener("mousedown", onMouseDown);
+
             window.removeEventListener("resize", resize);
         };
     }, []);
