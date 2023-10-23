32603822b08219b8a621a7b28ab04f39d769db2a
hyojinLeee-git <splato8899@gmail.com>
2023-10-23 23:27:02 +0900

    feat: 원 주변 경계 ground 구현

--- src/containers/RotateCanvas/index.jsx
+++ src/containers/RotateCanvas/index.jsx
@@ -1,10 +1,81 @@
-import { useRef } from "react";
-
+import { useRef, useEffect } from "react";
+import {
+    Engine,
+    Render,
+    Runner,
+    Mouse,
+    MouseConstraint,
+    Composite,
+    Bodies,
+} from "matter-js";
 import "./style.css";
 
 const RotateCanvas = () => {
     const canvasRef = useRef(null);
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const cw = 1000;
+        const ch = 1000;
 
+        let engine, render, runner, mouse, mouseConstraint;
+
+        initScene();
+        initMouse();
+        initGround();
+
+        canvas.addEventListener("mousewheel", () => {
+            addRect(mouse.position.x, mouse.position.y, 50, 50);
+        });
+
+        function initScene() {
+            engine = Engine.create();
+            render = Render.create({
+                canvas: canvas,
+                engine: engine,
+                options: {
+                    width: cw,
+                    height: ch,
+                    wireframes: false,
+                    background: "#1b1b19",
+                },
+            });
+            runner = Runner.create();
+
+            Render.run(render);
+            Runner.run(runner, engine);
+        }
+
+        function initMouse() {
+            mouse = Mouse.create(canvas);
+            mouseConstraint = MouseConstraint.create(engine, {
+                mouse: mouse,
+            });
+            Composite.add(engine.world, mouseConstraint);
+        }
+
+        function initGround() {
+            const segments = 32;
+            const deg = (Math.PI * 2) / segments;
+            const width = 50;
+            const radius = cw / 2 + width / 2;
+            const height = radius * Math.tan(deg / 2) * 2;
+
+            for (let i = 0; i < segments; i++) {
+                const theta = deg * i;
+                const x = radius * Math.cos(theta) + cw / 2;
+                const y = radius * Math.sin(theta) + ch / 2;
+                addRect(x, y, width, height, {
+                    isStatic: true,
+                    angle: theta,
+                });
+            }
+        }
+
+        function addRect(x, y, w, h, options = {}) {
+            const rect = Bodies.rectangle(x, y, w, h, options);
+            Composite.add(engine.world, rect);
+        }
+    }, []);
     return (
         <div className="rotate-canvas-wrapper">
             <canvas ref={canvasRef}></canvas>
